3974861cf2f02946f888066c30e09852dc57cb6b
eggplants <w10776e8w@yahoo.co.jp>
2021-09-18 23:39:22 +0900

    add: CLI (#3)

---
+++ alphabet2kana/main.py
@@ -0,0 +1,33 @@
+import argparse
+
+from .alphabet2kana import a2k
+
+
+def parse_args(test=None):
+    parser = argparse.ArgumentParser(
+        prog='a2k',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="Convert English alphabet to Katakana")
+    parser.add_argument('text', metavar='text', type=str,
+                        help='Half-width English alphabet string')
+    parser.add_argument('-d', '--delimiter', type=str,
+                        metavar='char',
+                        help='Katakana delimiter')
+    parser.add_argument('-n', '--numeral', action='store_true',
+                        help='Convert Arabic numerals')
+    parser.add_argument('-V', '--version', action='version',
+                        version="0.1.4")
+    if test:
+        return parser.parse_args(test)
+    else:
+        return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    res = a2k(args.text, delimiter=args.delimiter, numeral=args.numeral)
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
